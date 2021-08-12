c29d23d032ad2c5145df9b9d0765558b90384998
deepshetye <deepshetye1@gmail.com>
2021-08-12 21:28:02 +0530

    -a

--- frontend/src/components/people/People.js
+++ frontend/src/components/people/People.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
 import FacultyCard from './FacultyCard';
 import { AuthContext } from "../../Auth";
-import { getFacultyDetails } from '../../http';
+// import { getFacultyDetails } from '../../http';
 
 const People = () => {
 
     const { currentUserData } = useContext(AuthContext);
     const [facultyDetails, setFacultyDetails] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             const result = await getFacultyDetails(currentUserData);
             setFacultyDetails(result)
-            setLoading(false);
+            // setLoading(false);
         })()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
- 
+
     return (
         <div className="main_content_body">
             <FacultyCard />
