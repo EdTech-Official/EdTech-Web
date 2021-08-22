a352b347e7bef60cb09dbe11f42f596b28452ceb
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2021-08-23 03:02:20 +0530

    added environment var for secret key

--- edtech_dj/settings.py
+++ edtech_dj/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from typing import cast
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52sfempv%m$hh*ea34d!dp6=3of*qm&=b2p1m32o^4(0&iiezy'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
